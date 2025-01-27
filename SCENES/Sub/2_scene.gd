8c22470fa4d4bc1853c33dbfe33db356d45cf9a4
BaltazarAaron <166366120+aarroo14@users.noreply.github.com>
2025-01-27 21:01:57 +0800

    Cutscene (start)
    Just added 7 slides for the Cutscene (start). Also added a button (Next) on the lower right of the screen. And Slide counter on upper Left of the screen.

---
+++ SCENES/Sub/2_scene.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+
+
+func _on_skip_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://SCENES/Sub/3scene.tscn")
---
+++ SCENES/Sub/3_scene.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+
+
+func _on_skip_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://SCENES/Sub/4scene.tscn")
