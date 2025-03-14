2f2ec31d14c68f59e1a019bee4613ca77d658b92
Splash0907 <ruznielbulaong@yahoo.com>
2025-03-14 22:14:26 +0800

    Small Initial Tutorial Update
    Added a Initial tutorial before the game starts

--- SCENES/Sub/7_scene.gd
+++ SCENES/Sub/7_scene.gd
@@ -5,4 +5,4 @@ extends Node2D
 
 
 func _on_skip_7_pressed() -> void:
-	get_tree().change_scene_to_file("res://SCENES/Main/game.tscn")
+	get_tree().change_scene_to_file("res://SCENES/Sub/Game_Tutorial/Tutorial_INITIAL.tscn")
